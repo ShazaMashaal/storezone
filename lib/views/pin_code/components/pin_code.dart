479568dcf6d99e155d7896fed8b96ff1739c6477
Shaza <shazamashaal308@gmail.com>
2021-09-20 22:51:04 +0200

    .....

--- lib/views/pin_code/components/pin_code.dart
+++ lib/views/pin_code/components/pin_code.dart
@@ -1,27 +1,22 @@
-// import 'package:flutter/material.dart';
-// import 'package:pin_code_text_field/pin_code_text_field.dart';
-//
-// class PinCode extends StatelessWidget {
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Padding(
-//         padding:
-//         const EdgeInsets.symmetric(vertical: 10.0, horizontal: 50),
-//         child: PinCodeTextField(
-//           length: 4,
-//           obscureText: false,
-//           pinTheme: PinTheme(
-//             shape: PinCodeFieldShape.box,
-//             borderRadius: BorderRadius.circular(8),
-//             inactiveColor:  Color(0xFFE6E5E5),
-//             fieldHeight: 78,
-//             fieldWidth: 60,
-//             activeFillColor:Color(0xFFE6E5E5),
-//
-//
-//           ),
-//
-//         ));
-//   }
-// }
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pin_code_text_field/pin_code_text_field.dart';
+
+class PinCode extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PinCodeTextField(
+      pinBoxBorderWidth: 1,
+      defaultBorderColor: Colors.grey,
+      hasTextBorderColor: Colors.orangeAccent,
+      maxLength: 4,
+      autofocus: true,
+      pinBoxColor: Colors.grey[200],
+      pinTextStyle: TextStyle(fontSize: 22.0),
+      keyboardType: TextInputType.number,
+      pinBoxRadius: 10,
+      pinBoxHeight: MediaQuery.of(context).size.height/11,
+      pinBoxOuterPadding: EdgeInsets.all(10),
+    );
+  }
+}
--- lib/views/pin_code/view.dart
+++ lib/views/pin_code/view.dart
@@ -6,6 +6,7 @@ import 'package:storezone/translations/locale_keys.g.dart';
 import 'package:storezone/views/forgot_password/components/custom_appbar.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:storezone/widgets/custom_button.dart';
+import 'components/pin_code.dart';
 import 'components/rich_span.dart';
 import 'components/title.dart';
 
@@ -25,15 +26,7 @@ class PinCodeView extends StatelessWidget {
                 children: [
                   RichSpan(text: LocaleKeys.pinCode_enterCode.tr()+" ",boldText: LocaleKeys.login_email.tr(),boldColor: Colors.black,),
                   SizedBox(height: MediaQuery.of(context).size.height/14,),
-                  PinCodeTextField(
-                    maxLength: 4,
-                    autofocus: true,
-                    
-                    pinTextStyle: TextStyle(fontSize: 22.0),
-                    keyboardType: TextInputType.number,
-
-
-                  ),
+                  PinCode(),
                   SizedBox(height: MediaQuery.of(context).size.height/20,),
 
                   RichSpan(text: LocaleKeys.pinCode_didntRecieveCode.tr()+" ",boldText:LocaleKeys.pinCode_resend.tr(),boldColor: Color(0xFFFD9A25),),
